956cdca3f6c80ff0ee8de78b3137b5cf8c908635
jun <dudwns1852@naver.com>
2023-06-15 15:31:29 +0900

    login complete

--- src/main/java/com/playdata/todos/dto/User.java
+++ src/main/java/com/playdata/todos/dto/User.java
@@ -3,36 +3,55 @@ package com.playdata.todos.dto;
 public class User {
     private Integer id;
     private String username;
-    private String name;
     private String password;
+    private String name;
     private String createAt;
+
+    public User(Integer id, String username, String password, String name, String createAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.createAt = createAt;
+    }
+
     public Integer getId() {
         return id;
     }
 
-    public User(Integer id, String username, String name, String password, String createAt) {
+    public void setId(Integer id) {
         this.id = id;
-        this.username = username;
-        this.name = name;
-        this.password = password;
-        this.createAt = createAt;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public String getName() {
-        return name;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getCreateAt() {
         return createAt;
     }
 
-
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
 }
